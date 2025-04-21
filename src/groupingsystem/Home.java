608f718193dec10122f6affd8c3fa6396c81ba36
AaryaBalwadkar <aaryabalwadkar2005@outlook.com>
2025-04-21 10:23:04 +0530

    demo 2

--- src/groupingsystem/Home.java
+++ src/groupingsystem/Home.java
@@ -11,7 +11,7 @@ public class Home extends JFrame {
 
     public Home() {
         setTitle("Home");
-        setSize(1000, 700);
+        setSize(500, 700);
         setLocation(200, 60);
         setLayout(new BorderLayout()); // use BorderLayout to manage regions
 
